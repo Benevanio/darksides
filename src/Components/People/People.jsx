153b06454783bcba0ec2fc0afd9680bdd8458f49
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-13 13:40:11 -0300

    Finalizando a pagina de people

--- src/Components/People/People.jsx
+++ src/Components/People/People.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import Navbar from '../Navbar/Navbar';
+import './People.scss'
 
 const People = () => {
     const [people, setPeople] = useState([]);
@@ -22,11 +23,11 @@ const People = () => {
     return (
         <>
         <Navbar />
-            <div className="people">
-                <h1>People</h1>
+            <div className="card">
+                <h1 className="card__title">People</h1>
                 {people.map((person) => (
-                    <div key={person.name}>
-                        <h2>{person.name}</h2>
+                    <div key={person.name} className="card__content">
+                        <h2 className="card__content__name">{person.name}</h2>
                         <p>Height: {person.height}</p>
                         <p>Mass: {person.mass}</p>
                         <p>Hair Color: {person.hair_color}</p>
